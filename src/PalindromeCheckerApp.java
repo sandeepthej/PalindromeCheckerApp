d2f9620871af5213834d471fcb31fb7825e28ff8
sandeepthej <sandeepthej1707@gmail.com>
2026-02-26 10:47:33 +0530

    UC9:Recursive Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,24 +2,28 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        String input = "A man a plan a canal Panama";
+        String input = "madam";
 
-        // Step 1: Normalize string
-        String normalized = input.replaceAll("[^a-zA-Z]", "").toLowerCase();
+        boolean isPalindrome = check(input, 0, input.length() - 1);
 
-        boolean isPalindrome = true;
-
-        // Step 2: Compare characters from both ends
-        for (int i = 0; i < normalized.length() / 2; i++) {
-            if (normalized.charAt(i) !=
-                    normalized.charAt(normalized.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        // Output result
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+
+    // Recursive method
+    private static boolean check(String s, int start, int end) {
+
+        // Base condition: If start >= end, all characters matched
+        if (start >= end) {
+            return true;
+        }
+
+        // If mismatch found
+        if (s.charAt(start) != s.charAt(end)) {
+            return false;
+        }
+
+        // Recursive call moving inward
+        return check(s, start + 1, end - 1);
     }
 }
